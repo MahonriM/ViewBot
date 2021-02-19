7a91b9bef4e1f611bc33091acecc9b0e85c1dfaf
MahonriM <cripto3000z@gmail.com>
2021-02-18 22:14:06 -0600

    Add files via upload

---
+++ youtubepy.py
@@ -0,0 +1,11 @@
+import pywhatkit
+from time import  sleep
+try:
+    url="https://www.youtube.com/watch?v=MvodXLnd89o"
+    #"https://www.youtube.com/watch?v=Jp1p_cEpyR8"
+    while True:
+        sleep(30)
+        pywhatkit.playonyt(url)
+        print("Playing")
+except:
+    print("Network Error Occured")
