c78931f0bf17eb27d51fbe22ae1cf9993ad75170
MahonriM <cripto3000z@gmail.com>
2021-02-18 22:14:44 -0600

    Update youtubepy.py

--- youtubepy.py
+++ youtubepy.py
@@ -3,7 +3,7 @@ from time import  sleep
 try:
     url="https://www.youtube.com/watch?v=MvodXLnd89o"
     #"https://www.youtube.com/watch?v=Jp1p_cEpyR8"
-    while True:
+    while 10:
         sleep(30)
         pywhatkit.playonyt(url)
         print("Playing")
